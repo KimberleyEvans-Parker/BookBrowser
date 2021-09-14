ea0aa7027e702d2e3bfa7f176933f97c0018314e
Kimberley <kim@evans-parker.com>
2021-09-14 15:00:21 +1200

    Add set up for conftest

--- library/__init__.py
+++ library/__init__.py
@@ -1,21 +1,28 @@
 """Initialize Flask app."""
 
+from pathlib import Path
 from flask import Flask, render_template
 from library.adapters import jsondatareader
 
 from library.adapters.jsondatareader import BooksJSONReader
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
     
     # Configure the app from configuration-file settings.
     app.config.from_object('config.Config')
+    data_path = Path('library') / 'adapters' / 'data'
+
+    if test_config is not None:
+        # Load test configuration, and override any configuration settings.
+        app.config.from_mapping(test_config)
+        # data_path = app.config['TEST_DATA_PATH']
 
     # Initialise repo
-    jsondatareader.book_dataset = BooksJSONReader("library/adapters/data/comic_books_excerpt.json", 
-        "library/adapters/data/book_authors_excerpt.json", "library/adapters/data/book_inventory.json")
+    jsondatareader.book_dataset = BooksJSONReader(data_path/"comic_books_excerpt.json", 
+        data_path/"book_authors_excerpt.json", data_path/"book_inventory.json")
+
     jsondatareader.book_dataset.read_json_files()
-    print(len(jsondatareader.book_dataset.dataset_of_books), jsondatareader.book_dataset.dataset_of_books[0])
 
     
     with app.app_context():
