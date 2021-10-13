5e1adc5e8bbd1c7a4f8f42d44eccbda0885b2da3
Kimberley <kim@evans-parker.com>
2021-10-14 00:40:06 +1300

    add template for intialising repo via sql

---
+++ library/adapters/orm.py
@@ -0,0 +1,79 @@
+from sqlalchemy import (
+    Table, MetaData, Column, Integer, String, Date, DateTime,
+    ForeignKey
+)
+from sqlalchemy.orm import mapper, relationship, synonym
+
+from library.domain import model
+
+# global variable giving access to the MetaData (schema) information of the database
+metadata = MetaData()
+
+users_table = Table(
+    'users', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('user_name', String(255), unique=True, nullable=False),
+    Column('password', String(255), nullable=False)
+)
+
+reviews_table = Table(
+    'reviews', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('user_id', ForeignKey('users.id')),
+    Column('book_id', ForeignKey('books.id')),
+    Column('review', String(1024), nullable=False),
+    Column('timestamp', DateTime, nullable=False)
+)
+
+books_table = Table(
+    'books', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('date', Date, nullable=False),
+    Column('title', String(255), nullable=False),
+    Column('first_paragraph', String(1024), nullable=False),
+    Column('hyperlink', String(255), nullable=False),
+    Column('image_hyperlink', String(255), nullable=False)
+)
+
+tags_table = Table(
+    'tags', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('tag_name', String(64), nullable=False)
+)
+
+book_tags_table = Table(
+    'book_tags', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('book_id', ForeignKey('books.id')),
+    Column('tag_id', ForeignKey('tags.id'))
+)
+
+def map_model_to_tables():
+    mapper(model.User, users_table, properties={
+        '_User__user_name': users_table.c.user_name,
+        '_User__password': users_table.c.password,
+        '_User__reviews': relationship(model.Review, backref='_Review__user')
+    })
+    mapper(model.Review, reviews_table, properties={
+        '_Review__review': reviews_table.c.review,
+        '_Review__timestamp': reviews_table.c.timestamp
+    })
+    mapper(model.Book, books_table, properties={
+        '_Book__id': books_table.c.id,
+        '_Book__date': books_table.c.date,
+        '_Book__title': books_table.c.title,
+        '_Book__first_paragraph': books_table.c.first_paragraph,
+        '_Book__hyperlink': books_table.c.hyperlink,
+        '_Book__image_hyperlink': books_table.c.image_hyperlink,
+        '_Book__reviews': relationship(model.Review, backref='_Review__book'),
+        '_Book__tags': relationship(model.Tag, secondary=book_tags_table,
+                                       back_populates='_Tag__tagged_books')
+    })
+    mapper(model.Tag, tags_table, properties={
+        '_Tag__tag_name': tags_table.c.tag_name,
+        '_Tag__tagged_books': relationship(
+            model.Book,
+            secondary=book_tags_table,
+            back_populates="_Book__tags"
+        )
+    })
