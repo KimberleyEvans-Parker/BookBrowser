c3e948277cdb85aac2b7542c5541576b78346247
Ish Gill <igil215@aucklanduni.ac.nz>
2021-10-16 23:25:54 +1300

    Passed an initial test, ORM mapping seem to be working or at initial stages

--- library/adapters/orm.py
+++ library/adapters/orm.py
@@ -15,7 +15,7 @@ users_table = Table(
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('user_name', String(255), unique=True, nullable=False),
     Column('password', String(255), nullable=False),
-    Column('reading_list', String(255)), # TODO: Should this be multiple book objects?
+    Column('reading_list', String(255)), # TODO: Should this be multiple book objects? Yes I believe so.
 )
 
 # Books and authors have a one to many relationship.
@@ -23,7 +23,7 @@ users_table = Table(
 authors_table = Table(
     'authors', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('author_id', ForeignKey('books.book_id')),
+    Column('author_id', ForeignKey('books.book_id')),   # One book has many authors but a author only has one book.
     Column('full-name', String(255), nullable=False)
 )
 
@@ -37,8 +37,8 @@ reviews_table = Table(
     'reviews', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
     Column('user_name', String(255), nullable=False),
-    Column('user_id', ForeignKey('users.id')),
-    Column('book_id', ForeignKey('books.book_id')),
+    Column('user_id', ForeignKey('users.id')),  # One review has only one user but one user can have N reviews.
+    Column('book_id', ForeignKey('books.book_id')), # One review belongs to a single book, but one book can have N reviews.
     Column('rating', Integer, nullable=False),
     Column('review', String(1024), nullable=False),
     Column('timestamp', DateTime, nullable=False)
@@ -53,31 +53,33 @@ books_table = Table(
     Column('book_id', Integer, primary_key=True, autoincrement=True),
     Column('title', String(255), nullable=False),
     Column('description', String(1024), nullable=False),
-    Column('publisher', ForeignKey('publishers.id')), # TODO: Should this be a publisher object?
-    # Column('authors', String(1024)), # TODO: Should this be multiple author objects?
+    Column('publisher', String(1024)), # TODO: Should this be a publisher object? No Don't think so.
+    Column('authors', ForeignKey('authors.id')), # TODO: Should this be multiple author objects? Yes I think so as one book can have N authors
     Column('release_year', Date),
     Column('ebook', Boolean),
     Column('num_pages', String(63)),
     Column('average_rating', Float),
     Column('ratings_count', Integer),
     Column('url', String(255)),
-    # Column('reviews', ForeignKey('reviews.id')), # TODO: Should this be multiple review objects?
-    Column('user_id', ForeignKey('users.id')) # TODO: Is this required?  As in I know the user has a list of fav books, but idk if the books table needs to store the users
+    Column('reviews', String(1024)), # TODO: Should this be multiple review objects? I removed the ForeignKey here and it worked so let's see how it goes. :)
+    # Column('user_id', ForeignKey('users.id')) # TODO: Is this required?  As in I know the user has a list of fav books, but idk if the books table needs to store the users. I don't think the book needs to store the user either. Let's assume not for now.
 )
 
 reading_list_user_table = Table(
     'reading_list', metadata,
     Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('book_id', ForeignKey('books.book_id')),
-    Column('user_id', ForeignKey('users.id'))
+    Column('book_id', ForeignKey('books.book_id')),     # This implies that a single book can have many reading lists but a readlist can only have a single book. I don't think this holds.
+    Column('user_id', ForeignKey('users.id'))       # This implies that a single user can have many reading lists but a readinglist can only have a single user. I think it should be just a one to one relationship?
 )
 
 def map_model_to_tables():
+    # Mappers set up the relationship between the tables and instance variables in the domain model.
+    # In a one-to-many relationship such as user and review, we only define the relationship in the mapper class which holds the singularity.
+    # ie one user has many reviews, hence we define the relationship in the user mapper.
     mapper(model.User, users_table, properties={
         '_User__user_name': users_table.c.user_name,
         '_User__password': users_table.c.password,
-        '_User__reviews': relationship(model.Review, backref='_Review__user')
-
+        '_User__reviews': relationship(model.Review, backref='_Review__user_name') # User.__reviews is a list of review objects help by a user.
         # '_User__reading_list:' relationship(model.Book, backref='_')
     })
     mapper(model.Review, reviews_table, properties={
@@ -96,7 +98,7 @@ def map_model_to_tables():
         '_Book__average_rating': books_table.c.average_rating,
         '_Book__ratings_count': books_table.c.ratings_count,
         '_Book__url': books_table.c.url,
-        '_Book__reviews': relationship(model.Review, backref='_Review__book'),
+        '_Book__reviews': relationship(model.Review, backref='_Review__book_title'),    # There was no _Review__book instance variable in books so I made it _Review__book_title. Perhaps this will work.
         # '_Book__tags': relationship(model.Tag, secondary=book_tags_table,
         #                                back_populates='_Tag__tagged_books')
         # 
