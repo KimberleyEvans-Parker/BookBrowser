32f73ac95c598a5ee72809c49e8a87258f4e86e5
Ish Gill <igil215@aucklanduni.ac.nz>
2021-10-18 23:05:26 +1300

    more reviews

--- library/adapters/database_repository.py
+++ library/adapters/database_repository.py
@@ -105,6 +105,9 @@ class SqlAlchemyRepository(AbstractRepository):
     def get_first_author(self, book: Book) -> str:
         return book.authors[0].full_name
 
+    def get_number_authors(self, book: Book) -> int:
+        return len(book.authors)
+
     def get_date(self, book: Book) -> int:
         if book.release_year == None:
             return math.inf
